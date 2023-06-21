b2e21984b1dc2c2378d094fa8108d67bef896502
sivasankar.prithivirajan <sivasankar.prithivirajan@cloudsense.com>
2023-06-21 10:59:30 +0530

    First

---
+++ routes/userManagement.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var router = express.Router();
+const Realm = require("realm");
+
+const app = new Realm.App({
+    id: "usermanagement-yrxaw",
+  });
+
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('Welcome');
+});
+
+router.get('/registerUser',async (req,res)=>{
+  const token = req.query.token;
+  const tokenId = req.query.tokenId;
+  const conf = await app.emailPasswordAuth.confirmUser({ token, tokenId });
+  const credentials = Realm.Credentials.emailPassword("sivasankar53662@gmail.com", "Pa55w0rd!");
+  const user = await app.logIn(credentials);
+  res.send('Thanks for Confirmation!');
+})
+
+app.get('/resetPassword',async (req,res)=>{
+  res.send('resetPassword!');
+})
+
+module.exports = router;
